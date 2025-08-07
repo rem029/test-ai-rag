a8c64ff30d3f6e61206b41b695f8f9ff5950a987
Lawrence Ponce <elawrenceponce@gmail.com>
2025-08-07 11:17:34 +0300

    ignore .gguf files. add install script inside models

---
+++ models/install-models.sh
@@ -0,0 +1,10 @@
+# download qwen2.5VL
+wget https://huggingface.co/remyxai/SpaceThinker-Qwen2.5VL-3B/resolve/main/gguf/spacethinker-qwen2.5vl-3b-vision.gguf?download=true -O spacethinker-qwen2.5vl-3b-vision.gguf
+wget https://huggingface.co/remyxai/SpaceThinker-Qwen2.5VL-3B/resolve/main/gguf/spacethinker-qwen2.5VL-3B-q4_k_m.gguf?download=true -O spacethinker-qwen2.5VL-3B-q4_k_m.gguf
+
+# download gemma3-4b
+wget https://huggingface.co/lmstudio-community/gemma-3-4b-it-GGUF/resolve/main/gemma-3-4b-it-Q4_K_M.gguf?download=true -O gemma-3-4b-it-Q4_K_M.gguf
+wget https://huggingface.co/lmstudio-community/gemma-3-4b-it-GGUF/resolve/main/mmproj-model-f16.gguf?download=true -O mmproj-model-f16.gguf
+
+# download nomic-embed-text
+wget https://huggingface.co/nomic-ai/nomic-embed-text-v1.5-GGUF/resolve/main/nomic-embed-text-v1.5.Q4_K_M.gguf?download=true -O nomic-embed-text-v1.5.Q4_K_M.gguf
