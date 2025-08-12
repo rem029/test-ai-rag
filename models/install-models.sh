106a2222c08ec42d41dc3669964f87ea142aa45a
Lawrence Ponce <elawrenceponce@gmail.com>
2025-08-13 00:21:48 +0300

    Update environment and model installation scripts for new model filenames

--- models/install-models.sh
+++ models/install-models.sh
@@ -4,3 +4,7 @@ wget https://huggingface.co/lmstudio-community/gemma-3-4b-it-GGUF/resolve/main/m
 
 # download nomic-embed-text
 wget https://huggingface.co/nomic-ai/nomic-embed-text-v1.5-GGUF/resolve/main/nomic-embed-text-v1.5.Q4_K_M.gguf?download=true -O nomic-embed-text-v1.5.Q4_K_M.gguf
+
+# download qwen2.5VL 3b
+wget https://huggingface.co/unsloth/Qwen2.5-VL-3B-Instruct-GGUF/resolve/main/mmproj-F16.gguf?download=true -O mmproj-F16.gguf
+wget https://huggingface.co/unsloth/Qwen2.5-VL-3B-Instruct-GGUF/resolve/main/Qwen2.5-VL-3B-Instruct-Q4_K_M.gguf?download=true -O Qwen2.5-VL-3B-Instruct-Q4_K_M.gguf
